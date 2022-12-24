ea2ef577f66722f44564b832a2fc335f9387a6a0
everyzzz <jquisper2@gmail.com>
2022-12-24 13:44:35 -0500

    first commit

---
+++ js/login/login.js
@@ -0,0 +1,52 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+const urlLogin = 'http://127.0.0.1:8000/users/login/'
+
+form.onsubmit = async function(event) {
+    event.preventDefault();
+    const body = {}
+
+    inputs.forEach((input)=> (body[input.name]= input.value));
+    try {
+        const response = await fetch(urlLogin, {
+            method : "POST",
+            headers : {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body), 
+            
+        });
+        //console.log(body)
+        //console.log(response)
+        const data = await response.json();
+			if (data) {
+				console.log(data); //obteniendo datos
+				const setToken = data.access;
+                console.log(setToken) //obteniendo token
+            }
+        
+        if (data.id){
+            Swal.fire({
+                text : "Registrado correctamente",
+                icon : "success",
+                showConfirmButton: true,
+            });
+        }else if (body.email === "" || body.password === ""){
+            Swal.fire({
+                text : "Por favor, rellena todos los campos",
+                icon : "warning",
+                showConfirmButton: true,
+            });
+        }else{
+            Swal.fire({
+                title : "¡Error!",
+                text : "Usuario o contraseña incorrectos",
+                icon : "error",
+                showConfirmButton: true,
+            });
+        }
+    }catch(error){
+        console.log(error)
+    }
+};
---
+++ js/login/register.js
@@ -0,0 +1,43 @@
+const urlRegister = 'http://127.0.0.1:8000/users/signup/'
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+form.onsubmit = async function(event){
+    event.preventDefault();
+    const body = {}
+    
+    inputs.forEach((input) => (body[input.name] = input.value));
+    try{
+        const response = await fetch(urlRegister,{
+            method : "POST",
+            headers : {
+                "Content-type" : "application/json",
+            },
+            body : JSON.stringify(body),
+        });
+        //console.log(response)
+        //console.log(body)
+        if (response.ok){
+            Swal.fire({
+                text : "¡Usuario Registrado!",
+                icon : "success",
+                showConfirmButton: true,
+            }); 
+        }else if (body.email === "" || body.username === ""|| body.password === ""){
+            Swal.fire({
+                text : "¡Por favor, llena todos los campos!",
+                icon : "warning"
+            });
+        }
+        else{
+            Swal.fire({
+                title: 'Oops...',
+                text: '¡Algo salio mal!',
+                icon : "error"
+            });
+        }
+        
+    }catch(error){
+        console.log(error)
+    }
+};
